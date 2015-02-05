fcee232d65367320547173015b71d8a83c9a1bb4
Matthias De Jesus Oliveira <mde-jesu@student.42.fr>
2015-02-05 11:36:00 +0100

    fuck this LOL

--- srcs/ft_putnbr_fd.c
+++ srcs/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: mde-jesu <mde-jesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 18:31:36 by mde-jesu          #+#    #+#             */
-/*   Updated: 2013/11/29 16:50:07 by mde-jesu         ###   ########.fr       */
+/*   Updated: 2015/02/05 11:35:43 by mde-jesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,8 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	if (n > 9)
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
-	else
-		ft_putchar_fd('0' + n, fd);
+	char	*nb;
+
+	nb = ft_itoa(n);
+	ft_putstr_fd(nb, fd);
 }
