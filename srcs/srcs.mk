ac8daed5e24de87dc83f3ffd577e31cf9003325b
Matthias De Jesus Oliveira <mde-jesu@student.42.fr>
2017-11-11 16:36:15 +0100

    bicolor in futur

--- srcs/srcs.mk
+++ srcs/srcs.mk
@@ -6,12 +6,11 @@
 #    By: mde-jesu <mde-jesu@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/11/10 18:30:32 by mde-jesu          #+#    #+#              #
-#    Updated: 2017/11/11 15:52:41 by mde-jesu         ###   ########.fr        #
+#    Updated: 2017/11/11 16:35:44 by mde-jesu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #include $(SRCDIR)/AVL/srcs.mk
-#include $(SRCDIR)/bicolor/srcs.mk
 include $(SRCDIR)/cdlst/srcs.mk
 include $(SRCDIR)/is/srcs.mk
 include $(SRCDIR)/lst/srcs.mk
